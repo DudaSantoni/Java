13b22e744889e78686179d186e3c5d64134fb7d3
DudaSantoni <159277791+DudaSantoni@users.noreply.github.com>
2024-02-28 12:15:39 -0300

    primeiro commit

---
+++ Soma.java
@@ -0,0 +1,28 @@
+package primeiro_projeto;
+import java.util.Scanner;
+public class Soma {
+
+	
+	public static void main(String[] args) {
+         // Variáveis
+		double nome,idade,som;
+		 //Instaciar classe Scanner
+		Scanner ler = new Scanner(System.in);
+				
+		 // Entrada de dados
+				System.out.println("Informe o seu nome: );
+		= ler.nextDouble();
+		
+		System.out.println("Informe a sua idade);
+		b = ler.nextDouble();
+		
+		//Processamento
+		som = nome+dade
+		//Sáida
+		System.out.println("O resultado é : " + som);
+		ler.close();
+		
+		}
+
+	}
+
